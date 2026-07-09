f5fb9039c0d09976eb83bf41b2a57aab2bfc97e6
swaberry-tech <swarangi_ug_23@ece.nits.ac.in>
2026-07-09 22:33:10 +0530

    UART Top and Testbench

---
+++ rtl/uart_top.v
@@ -0,0 +1,47 @@
+module uart_top #(
+    parameter CLK_FREQ = 50_000_000 , //50 MHz
+    parameter BAUD_RATE = 115_200 
+) (
+    input clk ,
+    input rst_n,
+    
+    //TRANSMITTER 
+    input tx_start ,
+    input [7:0] tx_data ,
+    output tx_busy ,
+    output tx ,
+
+    //RECEIVER 
+    input rx ,
+    output [7:0] rx_data,
+    output rx_done
+);
+    uart_tx #(
+        .CLK_FREQ(CLK_FREQ) ,
+        .BAUD_RATE(BAUD_RATE)
+    ) transmitter (
+         .clk(clk),
+         .rst_n(rst_n),
+
+         .tx_start(tx_start),
+         .tx_data(tx_data),
+
+         .tx(tx),
+         .tx_busy(tx_busy)
+    );
+
+    uart_rx #(
+        .CLK_FREQ(CLK_FREQ),
+        .BAUD_RATE(BAUD_RATE)
+    ) receiver (
+
+        .clk(clk),
+        .rst_n(rst_n),
+
+        .rx(rx),
+
+        .rx_data(rx_data),
+        .rx_done(rx_done)
+);
+
+endmodule
---
+++ tb/tb_uart_top.v
@@ -0,0 +1,90 @@
+`timescale 1ns/1ps
+
+module uart_top_tb;
+
+parameter CLK_FREQ  = 50_000_000;
+parameter BAUD_RATE = 115200;
+
+reg clk;
+reg rst_n;
+
+// TRANSMITTER
+reg tx_start;
+reg [7:0] tx_data;
+wire tx_busy;
+wire tx;
+
+// RECEIVER
+wire rx;
+wire [7:0] rx_data;
+wire rx_done;
+
+// Loopback connection
+assign rx = tx;
+
+// DUT
+uart_top #(
+    .CLK_FREQ(CLK_FREQ),
+    .BAUD_RATE(BAUD_RATE)
+) dut (
+
+    .clk(clk),
+    .rst_n(rst_n),
+
+    .tx_start(tx_start),
+    .tx_data(tx_data),
+    .tx_busy(tx_busy),
+    .tx(tx),
+
+    .rx(rx),
+    .rx_data(rx_data),
+    .rx_done(rx_done)
+);
+
+// Dump waveform
+initial begin
+    $dumpfile("waves/uart_top.vcd");
+    $dumpvars(0, uart_top_tb);
+end
+
+// 50 MHz clock
+always #10 clk = ~clk;
+
+// Test
+initial begin
+
+    clk = 0;
+    rst_n = 0;
+    tx_start = 0;
+    tx_data = 8'h00;
+
+    #100;
+    rst_n = 1;
+
+    // Send A5
+    @(posedge clk);
+    tx_data = 8'hA5;
+    tx_start = 1;
+
+    @(posedge clk);
+    tx_start = 0;
+
+    wait(rx_done);
+
+    // Send 3C
+    @(posedge clk);
+    tx_data = 8'h3C;
+    tx_start = 1;
+
+    @(posedge clk);
+    tx_start = 0;
+
+    wait(rx_done);
+
+    #1000;
+
+    $finish;
+
+end
+
+endmodule
